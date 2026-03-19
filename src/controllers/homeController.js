4843a85fd16d521577147a8d62ed4143cb1dcaf4
Martin Forbelský <mforbelak@gmail.com>
2026-03-19 10:40:08 +0100

    přidání vytváření a mazání views

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,56 @@
+const Note = require('../models/Note');
+
 exports.getHome = (req, res) => {
-    res.render('login', { 
-        title: 'Maturitní Cvičení JS - MVC',
-        message: 'Vítejte v naší aplikaci používající MVC architekturu!'
-    });
+    if (req.session.userId) {
+        return res.redirect('/notes');
+    }
+    res.redirect('/user/login');
 };
 
-exports.getNotes = (req, res) => {
+exports.getNotes = async (req, res) => {
     if (!req.session.userId) {
         return res.redirect('/user/login');
     }
     
-    res.render('notes', { 
-        title: 'Moje Poznámky',
-        user: req.session.username
-    });
+    try {
+        const notes = await Note.find({ user: req.session.userId }).sort({ createdAt: -1 });
+        res.render('notes', { 
+            title: 'Moje Poznámky',
+            user: req.session.username,
+            notes: notes
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Chyba při načítání poznámek');
+    }
+};
+
+exports.postNote = async (req, res) => {
+    if (!req.session.userId) return res.status(401).send('Nepřihlášen');
+    
+    try {
+        const { title, content } = req.body;
+        const newNote = new Note({
+            title,
+            content,
+            user: req.session.userId
+        });
+        await newNote.save();
+        res.redirect('/notes');
+    } catch (err) {
+        console.error(err);
+        res.redirect('/notes');
+    }
+};
+
+exports.deleteNote = async (req, res) => {
+    if (!req.session.userId) return res.status(401).send('Nepřihlášen');
+    
+    try {
+        await Note.findOneAndDelete({ _id: req.params.id, user: req.session.userId });
+        res.redirect('/notes');
+    } catch (err) {
+        console.error(err);
+        res.redirect('/notes');
+    }
 };
--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -4,5 +4,7 @@ const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.getHome);
 router.get('/notes', homeController.getNotes);
+router.post('/notes', homeController.postNote);
+router.get('/notes/delete/:id', homeController.deleteNote);
 
 module.exports = router;
